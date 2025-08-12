0a0c9f8043b3802b00c5127de87e0e90387553cd
salemmohdmohd <salim.farouq@gmail.com>
2025-08-11 19:12:11 -0500

    a

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import CardContainer from "../components/CardContainer.jsx";
-
-export const Home = () => {
-	return (
-		<div className="text-center mt-5 ">
-			<CardContainer />
-		</div>
-	);
-}; 
